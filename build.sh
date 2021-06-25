2d4eeb3662e257c01f6f0789de642f7eb72c6dbe
Jamie Wilkinson <jamie@jamiedubs.com>
2021-06-25 14:30:27 -0400

    Publish to Docker Hub with PUBLISH=1

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# 
+# to publish to docker hub, try:
+# PUBLISH=1 ./build.sh
+# 
+set -e
 
 image="jamiedubs/validator-exporter" # must match hub.docker.com repo name
 tag="latest"
 
 docker build -t "$image" .
-# docker push $image:$tag
+
+if [ -n "$PUBLISH" ]; then
+  echo "Publishing to Docker Hub..."
+  docker push $image:$tag
+fi
