d0415e79912280fa122d6978017f40e53f4999d4
Jamie Wilkinson <jamie@jamiedubs.com>
2021-07-03 11:45:25 -0400

    Run save-stats against a kube, not local docker

--- save-stats.sh
+++ save-stats.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
+# TODO how do we keep this in sync with `validator-setup.sh` from other repo?
+# right now these are duplicating one another and falling out of sync
 
-container="$1"
-
-if [ -z $container ]; then
-  echo "$0: specify container name as first argument"
+pod="$1"
+if [ -z $pod ]; then
+  echo "$0: specify pod name as first argument"
   exit 1
 fi
 
-echo "container=$container"
-v="docker exec $container miner"
+dir=${STATS_DIR:="./stats"}
+if [ ! -d "$dir" ]; then
+  echo "$0: STATS_DIR '$stats_dir' is not a directory"
+  exit 1
+fi
+
+container="validator"
+namespace="helium"
+v="time kubectl exec $pod -c $container -n $namespace -- miner"
 echo "v=$v"
-# dir=$STATS_DIR
-dir="$HOME/dev/validator-exporter/stats"
 
-echo "putting files into $dir ..."
+# paste from k8s/validator-setup.sh here
+# forgive me for what i've done
 
-date
-time $v info name > $dir/info_name;
-time $v info height > $dir/info_height;
-time $v info p2p_status > $dir/info_p2p_status;
-time $v info in_consensus > $dir/info_in_consensus;
-time $v info block_age > $dir/info_block_age;
-time $v hbbft perf --format csv > $dir/hbbft_perf.csv;
-time $v peer book -s --format csv > $dir/peer_book.csv;
-# time $v ledger validators --format csv > $dir/ledger_validators.csv;
-time $v print_keys > $dir/print_keys;
-time $v versions > $dir/versions;
-date
+echo "$(date): saving stats to $dir ..."
+start_time="$(date -u +%s)"
+$v info name > $dir/info_name;
+$v info height > $dir/info_height;
+$v info p2p_status > $dir/info_p2p_status;
+$v info in_consensus > $dir/info_in_consensus;
+$v info block_age > $dir/info_block_age;
+$v hbbft perf --format csv > $dir/hbbft_perf.csv;
+$v peer book -s --format csv > $dir/peer_book.csv;
+$v ledger validators --format csv > $dir/ledger_validators.csv;
+$v print_keys > $dir/print_keys;
+$v versions > $dir/versions;
+end_time="$(date -u +%s)"
+elapsed="$(($end_time-$start_time))"
+echo "Took $elapsed seconds"
