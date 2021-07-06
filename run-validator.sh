165cadc5e0c9aaefe2dd89b64ba5ba3c56d7fd48
Jamie Wilkinson <jamie@jamiedubs.com>
2021-07-06 15:49:54 -0400

    Expose JSON-RPC port

--- run-validator.sh
+++ run-validator.sh
@@ -23,6 +23,7 @@ docker rm -f $container_name
 # --restart always \
 docker run -d \
   --publish 2154:2154/tcp \
+  --publish 4467:4467/tcp \
   --name "$container_name" \
   --mount type=bind,source=$data_dir,target=/var/data \
   $image
