b0871207dcbc20be73b49199cdee1dfa1580f898
Jamie Wilkinson <jamie@jamiedubs.com>
2021-07-10 07:58:55 -0400

    Only save stats during test run with SAVE_STATS=1

--- test-run.sh
+++ test-run.sh
@@ -5,10 +5,13 @@ export POD_NAME=validator-test-0
 export NODE_NAME=imacomputer
 export DEBUG=1
 
-# find a suitable-ish running docker container
-# j/k let's just use kube pod names instead
-# container="docker ps --format '{{ .Names }}' | grep validator-0 | grep -v exporter"
-container="validator-0"
+if [ ! -z $SAVE_STATS ]; then
+  echo "Saving some fresh stats..."
+  # find a suitable-ish running docker container
+  # container="docker ps --format '{{ .Names }}' | grep validator-0 | grep -v exporter"
+  # j/k let's just use kube pod names instead
+  container="validator-0"
+fi
 
 ./save-stats.sh $container
 ./validator-exporter.py
